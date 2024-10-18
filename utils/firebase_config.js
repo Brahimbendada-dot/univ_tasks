8396af379fb8cc3b1c7e0733bde41bc0594c8fb5
brahimbendada-dot <brahimbendada024@gmail.com>
2024-10-18 16:17:44 +0200

    delete privat_key.json and integrate them by code

--- utils/firebase_config.js
+++ utils/firebase_config.js
@@ -1,9 +1,13 @@
 const admin = require('firebase-admin')
-const serviceAccount = require('../config/note-3f2f5-firebase-adminsdk-mwkha-96d70b6207.json')
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    storageBucket: "note-3f2f5.appspot.com"
-})
-const bucket = admin.storage().bucket()
-module.exports = bucket
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+  }),
+  storageBucket: "note-3f2f5.appspot.com"
+});
+
+const bucket = admin.storage().bucket();
+module.exports = bucket;
